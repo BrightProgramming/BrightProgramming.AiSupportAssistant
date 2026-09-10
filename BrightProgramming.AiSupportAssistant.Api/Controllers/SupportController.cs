2c042aabba651feb7915e96cff9136a0d094348b
IvorB <IvorB@IVORRGB>
2026-09-10 17:34:08 +0100

    Add project files.

---
+++ BrightProgramming.AiSupportAssistant.Api/Controllers/SupportController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrightProgramming.AiSupportAssistant.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SupportController : ControllerBase
+    {
+    }
+}
